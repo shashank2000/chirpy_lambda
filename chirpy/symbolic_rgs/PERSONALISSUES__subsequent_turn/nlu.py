279487782190f7d450aad5ad3f0c4a75d965c99e
jrmkim50 <jrmkim50@gmail.com>
2022-12-20 17:53:32 -0500

    Using double underscores for flags

--- chirpy/symbolic_rgs/PERSONALISSUES__subsequent_turn/nlu.py
+++ chirpy/symbolic_rgs/PERSONALISSUES__subsequent_turn/nlu.py
@@ -4,15 +4,15 @@ from chirpy.response_generators.personal_issues import personal_issues_helpers
 @nlu_processing
 def get_flags(rg, state, utterance):
     if personal_issues_helpers.is_gratitude_response(rg, utterance):
-        ADD_NLU_FLAG('PERSONALISSUE_gratitude')
+        ADD_NLU_FLAG('PERSONALISSUE__gratitude')
     if personal_issues_helpers.is_personal_issue(rg, utterance):
-        ADD_NLU_FLAG('PERSONALISSUE_personal_sharing_negative')
+        ADD_NLU_FLAG('PERSONALISSUE__personal_sharing_negative')
     if personal_issues_helpers.is_continued_sharing(rg, utterance):
-        ADD_NLU_FLAG('PERSONALISSUE_is_continued_sharing')
+        ADD_NLU_FLAG('PERSONALISSUE__is_continued_sharing')
     if personal_issues_helpers.is_noncommittal_response(rg, utterance):
-        ADD_NLU_FLAG('PERSONALISSUE_noncommittal')
+        ADD_NLU_FLAG('PERSONALISSUE__noncommittal')
     if personal_issues_helpers.is_short_response(rg, utterance):
-        ADD_NLU_FLAG('PERSONALISSUE_short_response')
+        ADD_NLU_FLAG('PERSONALISSUE__short_response')
 
 @nlu_processing
 def get_background_flags(rg, utterance):
