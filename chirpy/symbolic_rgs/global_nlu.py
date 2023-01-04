6db61aea15a2ff4bd0c10c311d9d823038a1f8ca
jrmkim50 <jrmkim50@gmail.com>
2023-01-04 14:20:21 -0500

    Added flag that saves the current entity if there is positive nav intent

--- chirpy/symbolic_rgs/global_nlu.py
+++ chirpy/symbolic_rgs/global_nlu.py
@@ -3,7 +3,10 @@ from chirpy.core.response_generator.nlu import *
 @nlu_processing
 def get_flags(context):
 	utterance = context.utterance
+	current_state = context.state_manager.current_state
 	if 'what' in utterance:
 		ADD_NLU_FLAG("GlobalFlag__WHAT")
 	if 'why' in utterance:
 		ADD_NLU_FLAG("GlobalFlag__WHY")
+	if current_state.entity_tracker.cur_entity and current_state.navigational_intent.pos_intent:
+		ADD_NLU_FLAG("GlobalFlag__SpecifiedEntity", current_state.entity_tracker.cur_entity)
