89696d77f260b72932e0a036ffcefadf560887c8
jrmkim50 <jrmkim50@gmail.com>
2023-01-08 20:39:52 -0800

    Added entitygroupregex class: takes in a regex template and if the template matches the utterance, it returns true. Added entitygroupregexlist class: if any of the regexes match, returns true. Fixed entitygrouplist to take a list of entitygroups. Fixed entitygroup to check for pos navigational intent

--- chirpy/core/camel/entitygroup.py
+++ chirpy/core/camel/entitygroup.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass, field
 from typing import List
 
 from chirpy.core.entity_linker.entity_groups import EntityGroup as _EntityGroup, EntityGroupsForExpectedType
+from chirpy.core.regex.regex_template import RegexTemplate
+from chirpy.core.response_generator.helpers import global_templates_cache
 
 
 @dataclass
@@ -10,15 +12,35 @@ class EntityGroup():
 		def __init__(self, entityGroupName: str) -> None:
 				self.entityGroup = getattr(EntityGroupsForExpectedType, entityGroupName)
 		def evaluate(self, context):
-				if not context.utilities["cur_entity"]:
+				if not context.flags["GlobalFlag__SpecifiedEntity"]:
 						return False
-				return self.entityGroup.matches(context.utilities["cur_entity"])
+				return self.entityGroup.matches(context.flags["GlobalFlag__SpecifiedEntity"])
+
+@dataclass
+class EntityGroupRegex():
+		entityRegex : RegexTemplate
+		def __init__(self, entityRegexName: str) -> None:
+				self.entityRegex = global_templates_cache[entityRegexName]
+		def evaluate(self, context):
+				slots = self.entityRegex.execute(context.utterance)
+				if not slots:
+					return False
+				return True
 
 @dataclass
 class EntityGroupList():
-		entityGroups : List[_EntityGroup] = field(default_factory=list)
+		entityGroups : List[EntityGroup] = field(default_factory=list)
 		def evaluate(self, context):
 				for entity in self.entityGroups:
+						if entity.evaluate(context):
+								return True
+				return False
+
+@dataclass
+class EntityGroupRegexList():
+		entityRegexes : List[EntityGroupRegex] = field(default_factory=list)
+		def evaluate(self, context):
+				for entity in self.entityRegexes:
 						if entity.evaluate(context):
 								return True
 				return False
