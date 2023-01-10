8f896f37e7aa7c12c3fa9be76df6067400bdae6b
siyan-sylvia-li <lisiyansylvia@gmail.com>
2023-01-10 10:16:30 -0800

    Preliminary generation files

--- chirpy/response_generators/celeb/es_celeb.py
+++ chirpy/response_generators/celeb/es_celeb.py
@@ -27,13 +27,16 @@ def gen_list_of_terms():
     times = ['20th', '21st']
     nations = ['American', 'British', 'English', 'Indian', 'Japanese', 'French', 'Spanish', 'Italian', 'Australian',
                'German', 'Mexican', 'Swedish', 'Danish']
-    professions = ['male actors', 'female actors', 'actresses', 'singers', 'rappers', 'YouTubers', 'bloggers']
+    professions = ['male actors', 'female actors', 'actresses', 'singers', 'rappers', 'YouTubers', 'bloggers', 'male models', 'female models', 'socialites', 'comedians']
     template = "{tms}-century {nation} {prof}"
     all_temps = []
     for t in times:
         for n in nations:
             for p in professions:
                 all_temps.append(template.format(tms=t, nation=n, prof=p))
+    for n in nations:
+        for p in professions:
+            all_temps.append(n + " " + p)
     return all_temps
 
 
@@ -48,10 +51,22 @@ def scrape_es():
                                  timeout=ANCHORTEXT_QUERY_TIMEOUT,
                                  filter_path=['hits.hits._source.{}'.format(field) for field in FIELDS_FILTER])
         for s in results:
-            all_celebs.append(s['_source']['doc_title'])
-    all_celebs = list(set(all_celebs))
+            all_celebs.append((s['_source']['doc_title'], s['_source']['pageview'], t))
     print(len(all_celebs))
-    return all_celebs
+    # get top 300 for each category
+    tops_celebs = {}
+    for t in all_temps:
+        core_profession = t.split(" ")[-1]
+        if core_profession not in tops_celebs:
+            tops_celebs.update({core_profession: []})
+        curr_list_celebs = [x for x in all_celebs if x[2] == t]
+        curr_list_celebs = [(x[0], x[1]) for x in curr_list_celebs]
+        tops_celebs[core_profession].extend(curr_list_celebs)
+    for c in tops_celebs:
+        tops_celebs[c] = list(set(tops_celebs[c]))
+        tops_celebs[c].sort(key=lambda x: x[1], reverse=True)
+        tops_celebs[c] = tops_celebs[c][:300]
+    return tops_celebs
 
 
 def filter_entities(ent):
@@ -83,15 +98,14 @@ def run_test_es():
     print(results)
 
 
-if __name__ == "__main__":
-    es = get_elasticsearch()
+def filter_all_celebs():
     filtered_celeb = {}
     all_celeb_info = json.load(open("all_celeb_info.json"))
 
     for c in tqdm.tqdm(all_celeb_info):
         filtered_celeb.update({c: {}})
         for k in all_celeb_info[c]:
-            if k != "pronoun":
+            if k != "pronoun" and k != "total_pg":
                 filtered_celeb[c].update({k: []})
                 for e in all_celeb_info[c][k]:
                     if filter_entities(e[0]):
@@ -99,7 +113,12 @@ if __name__ == "__main__":
             else:
                 filtered_celeb[c].update({k: all_celeb_info[c][k]})
 
-    json.dump(filtered_celeb, open("filtered_celeb.json", "w+"))
+    json.dump(filtered_celeb, open("filtered_celebs.json", "w+"))
 
 
+if __name__ == "__main__":
+    es = get_elasticsearch()
+    # all_celebs = scrape_es()
+    # pickle.dump(all_celebs, open("scraped_celebs.p", "wb+"))
+    filter_all_celebs()
 
---
+++ chirpy/response_generators/celeb/merge_celebs.py
@@ -0,0 +1,33 @@
+import json
+
+
+def merge_opinions():
+    celeb_opinion_file = open("celeb_opinions.jsonl").readlines()
+    merged_celeb_ops = {}
+    for l in celeb_opinion_file:
+        ld = json.loads(l)
+        merged_celeb_ops.update(ld)
+    return merged_celeb_ops
+
+
+def merge_opinions_work(celeb_keys):
+    celeb_keys = {c.lower(): c for c in celeb_keys}
+    celeb_opinion_work_file = open("celeb_opinions_work.jsonl").readlines()
+    merged_celeb_work_ops = {}
+    for l in celeb_opinion_work_file:
+        ld = json.loads(l)
+        if ld['work_name'] not in merged_celeb_work_ops:
+            merged_celeb_work_ops.update({ld['work_name']: {}})
+        if ld['celeb'] in celeb_keys:
+            merged_celeb_work_ops[ld['work_name']].update({
+                celeb_keys[ld['celeb']]: ld['opinion']
+            })
+    return merged_celeb_work_ops
+
+
+if __name__ == "__main__":
+    merge_celeb_ops = merge_opinions()
+    json.dump(merge_celeb_ops, open("individual_celeb_opinions.json", "w+"))
+    merge_celeb_work_ops = merge_opinions_work(merge_celeb_ops)
+    json.dump(merge_celeb_work_ops, open("celeb_work_opinions.json", "w+"))
+
