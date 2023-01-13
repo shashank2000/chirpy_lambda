8e23eea2db43f4c92c6be5bc3d663b02f48104ae
Scott Hickmann <scotthickmann21@gmail.com>
2023-01-13 14:56:22 -0800

    Fix auto talkable for wiki entity

--- chirpy/core/camel/nlg.py
+++ chirpy/core/camel/nlg.py
@@ -213,7 +213,7 @@ def spacingaware_join(x):
     for idx, item in enumerate(x):
         if not isinstance(item, str):
             if isinstance(item, WikiEntity):
-                item = item.talkable
+                item = item.name
             elif isinstance(item, [int, float]):
                 item = item.talkable
             else:
