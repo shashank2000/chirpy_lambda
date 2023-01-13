d05a5bd18429567cba4c1b48f5b825597bac1a03
shashank2000 <sr22@stanford.edu>
2023-01-12 20:48:22 -0800

    fixed capitalization issues

--- chirpy/databases/datalib/animal_database.py
+++ chirpy/databases/datalib/animal_database.py
@@ -15,17 +15,8 @@ with open(ANIMAL_GPT_DATABASE_FILE) as f:
 
 @database_exists("animal_gpt")
 def verify_animal_exists_gpt(animal_name : str):
-    # TODO: less hacky
-    if animal_name not in animal_gpt:
-        animal_name = animal_name[:-1]
     return animal_name in animal_gpt.keys()
 
 @database_lookup("animal_gpt")
 def lookup_animal_gpt(animal_name : str):
-    key = animal_name
-    if animal_name not in animal_gpt:
-        # hacky way to do sg/pl
-        # TODO (Shashank): use singularize_pluralnoun logic from entitylinker after speaking with Haojun
-        if animal_name[:-1] in animal_gpt:
-            key = animal_name[:-1]
-    return animal_gpt[key]
+    return animal_gpt[animal_name]
