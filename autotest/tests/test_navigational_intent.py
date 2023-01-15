f8bcbe215735766892fbf6b49aa4074718fb0578
Ethan Chi <ethanachi@gmail.com>
2023-01-15 17:32:26 -0500

    fix lookup in test

--- autotest/tests/test_navigational_intent.py
+++ autotest/tests/test_navigational_intent.py
@@ -7,4 +7,4 @@ class TestPrioritizeSupernode(BaseIntegrationTest):
     def test_prioritize_supernode(self, prioritized_supernode):
         """This test sets the score of the supernode to +1e10 so that it has a ~1e-10 chance of failing."""
         bot = self.startup_bot(launch_script=True, prioritized_supernode=prioritized_supernode)
-        assert bot.supernodes[prioritized_supernode]["chosen"] != False
+        assert prioritized_supernode in bot.supernodes and bot.supernodes[prioritized_supernode]["chosen"] != False
