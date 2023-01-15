66f5c89f6f060f9be5788afe4433e9740caf3bba
Ethan Chi <ethanachi@gmail.com>
2023-01-14 19:20:01 -0500

    add randomized ranking

--- bluejay/client/src/Logs.js
+++ bluejay/client/src/Logs.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 
+const escapeHtml = (unsafe) => {
+    return unsafe.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;');
+}
+
 const LogsPanel = (props) => {
 	console.log(props.currMessage.rg_state);
 	
 	const openLogs = () => {
 		console.log("openLogs");
-		let msg = props.currMessage.full_logs.join("<br/><br/>");
+		let msg = props.currMessage.full_logs.map((log) => escapeHtml(log)).join("<br/><br/>");
 		msg = msg.replace("\n", "<br/>");
 		
 		var wnd = window.open("about:blank", "", "_blank");
