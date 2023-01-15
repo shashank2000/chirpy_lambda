4c43ba1845f9c14a8a9ee4420e959635d4a6d3eb
JiHunWang <46038281+JiHunWang@users.noreply.github.com>
2023-01-15 12:22:40 -0800

    Update nlu.py

--- chirpy/symbolic_rgs/MOVIE__intro/nlu.py
+++ chirpy/symbolic_rgs/MOVIE__intro/nlu.py
@@ -1,4 +1,3 @@
-# from chirpy.response_generators.movie import movie_helpers
 from chirpy.core.response_generator.nlu import nlu_processing
 import logging
 
@@ -8,8 +7,8 @@ logger = logging.getLogger('chirpylogger')
 @nlu_processing
 def get_flags(context):
     pos_val = context.flags['GlobalFlag__YES']
-    logger.warning(f'User responded {pos_val} to the question "Do you like watching movies?"')
     ADD_NLU_FLAG('MOVIE__user_likes_movie', pos_val)
+
 
 @nlu_processing
 def get_background_flags(context):
