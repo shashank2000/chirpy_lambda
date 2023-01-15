b85a4fe287435560c550d582a04b0b37fa5d9ebb
JiHunWang <46038281+JiHunWang@users.noreply.github.com>
2023-01-15 12:25:40 -0800

    Update nlu.py

--- chirpy/symbolic_rgs/MOVIE__favourite/nlu.py
+++ chirpy/symbolic_rgs/MOVIE__favourite/nlu.py
@@ -2,18 +2,19 @@ from chirpy.response_generators.food import food_helpers
 from chirpy.core.response_generator.nlu import nlu_processing
 
 
-# check if move is True
+# check if movie is True
 def real_movie(movie):
     return True
+
 
 @nlu_processing
 def get_flags(context):
     entity = context.utilities["cur_entity"]
-    if entity is None: return
-    
-    #entity_name = entity.name.lower()
+    if entity is None: 
+        return
     if real_movie(entity):
         ADD_NLU_FLAG('MOVIE__user_mentioned_movie', True)
+
 
 @nlu_processing
 def get_background_flags(context):
