fbed1e93b4031e5dc9025a2a6ce31b9f5c906246
JiHunWang <46038281+JiHunWang@users.noreply.github.com>
2023-01-15 12:20:17 -0800

    Update movie_database.py

--- chirpy/databases/datalib/movie_database.py
+++ chirpy/databases/datalib/movie_database.py
@@ -18,48 +18,34 @@ with open(MOVIE_INFO_FILE) as f:
 with open(ACTOR_INFO_FILE) as f:
     actor_info = json.load(f)
 
+
 @database_exists("movie_info")
-def verify_movie_exists(movie_name : str):
+def verify_movie_exists(movie_name: str):
     return movie_name in movie_info.keys()
 
+
 @database_lookup("movie_info")
-def lookup_movie(movie_name : str):
+def lookup_movie(movie_name: str):
     if verify_movie_exists(movie_name):
         return movie_info[movie_name]
+    return {
+        "main_actor": None, 
+        "director": None, 
+        "genre": None, 
+        "plot": None, 
+        "most_memorable_scene": None
+    }
 
-    return {"main_actor": None, "director": None, "genre": None, "plot":None, "most_memorable_scene":None}
 
 @database_exists("actor_info")
-def verify_actor_exists(actor_name : str):
+def verify_actor_exists(actor_name: str):
     return actor_name in actor_info.keys()
 
+
 @database_lookup("actor_info")
-def lookup_actor(actor_name : str):
+def lookup_actor(actor_name: str):
     if verify_actor_exists(actor_name):
         return actor_info[actor_name]
-
-    return {"movie_name": None}
-
-# @database_exists("food_gpt")
-# def verify_food_exists_gpt(food_name : str):
-#     return food_name in food_gpt.keys()
-#
-# @database_lookup("food_gpt")
-# def lookup_food_gpt(food_name : str):
-#     return food_gpt[food_name]
-
-# @database_lookup("movie_info")
-# def lookup_movie_director(movie_name : str):
-#     return movie_info[movie_name]["director"]
-#
-# @database_lookup("movie_info")
-# def lookup_movie_genre(movie_name : str):
-#     return movie_info[movie_name]["genre"]
-#
-# @database_lookup("movie_info")
-# def lookup_movie_plot(movie_name : str):
-#     return movie_info[movie_name]["plot"]
-#
-# @database_lookup("movie_info")
-# def lookup_movie_scene(movie_name : str):
-#     return movie_info[movie_name]["most_memorable_scene"]
+    return {
+        "movie_name": None
+    }
