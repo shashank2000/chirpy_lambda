d52a9ca5ef083fa72c429ec252265ad461765e93
JiHunWang <46038281+JiHunWang@users.noreply.github.com>
2023-01-15 12:21:45 -0800

    Update nlg_helpers.py

--- chirpy/symbolic_rgs/MOVIE__intro/nlg_helpers.py
+++ chirpy/symbolic_rgs/MOVIE__intro/nlg_helpers.py
@@ -1,17 +1,12 @@
-# from chirpy.response_generators.movie import movie_helpers
 from chirpy.core.response_generator import nlg_helper
-# from chirpy.response_generators.movie.expression_lists import YES, NO
-
 import logging
 import re
 
 logger = logging.getLogger('chirpylogger')
 
 
-
 @nlg_helper
 def set_whether_user_likes_movie(flag):
-    logger.warning('flag is %s' % flag)
     if flag:
         logger.primary_info('Setting state to True!')
     else:
