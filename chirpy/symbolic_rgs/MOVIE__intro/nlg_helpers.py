ab237dd44274b714cb92f9a72b67eec3d035a868
jrmkim50 <jrmkim50@gmail.com>
2023-01-19 10:17:23 -0800

    Refactored movie nodes

--- chirpy/symbolic_rgs/MOVIE__intro/nlg_helpers.py
+++ chirpy/symbolic_rgs/MOVIE__intro/nlg_helpers.py
@@ -1,5 +1,6 @@
 from chirpy.response_generators.movie import movie_helpers
 from chirpy.core.response_generator import nlg_helper
+from chirpy.core.entity_linker.entity_groups import EntityGroupsForExpectedType
 
 import logging
 import re
@@ -8,9 +9,9 @@ logger = logging.getLogger("chirpylogger")
 
 
 @nlg_helper
-def set_whether_user_likes_movie(flag):
-    if flag:
-        logger.primary_info("Setting state to True!")
-    else:
-        logger.primary_info("Setting state to False!")
-    return flag
+def set_favorite_movie(cur_entity):
+    if not cur_entity:
+        return None
+    if EntityGroupsForExpectedType.film.matches(cur_entity):
+        return cur_entity
+    return None
--- chirpy/symbolic_rgs/OPINION__know_person_likes_activity/nlu.py
+++
@@ -1,5 +0,0 @@
-from chirpy.core.response_generator.nlu import nlu_processing
-
-@nlu_processing
-def get_flags(context):
-    return
