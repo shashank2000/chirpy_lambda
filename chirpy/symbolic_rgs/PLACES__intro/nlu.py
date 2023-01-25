ee92e5bac5bf406cdabab4b872dc6f0c855a2f0b
jrmkim50 <jrmkim50@gmail.com>
2023-01-25 09:24:51 -0800

    Added navigation for places

--- chirpy/symbolic_rgs/PLACES__intro/nlu.py
+++ chirpy/symbolic_rgs/PLACES__intro/nlu.py
@@ -1,5 +1,6 @@
 import pickle as pkl
 from chirpy.core.response_generator.nlu import nlu_processing
+from chirpy.core.entity_linker.entity_groups import EntityGroupsForExpectedType
 import os
 
 print(os.getcwd())
@@ -11,10 +12,7 @@ city_info = pkl.load(open(CITY_INFO_PATH, "rb"))
 def get_flags(context):
     entity = context.utilities["cur_entity"]
     if entity is None: return
-    
-    entity_name = entity.name.lower()
-    is_known_city = entity_name in city_info
-    if is_known_city:
+    if EntityGroupsForExpectedType.location_related.matches(entity):
         ADD_NLU_FLAG('PLACES__user_mentioned_city') 
         
 @nlu_processing
