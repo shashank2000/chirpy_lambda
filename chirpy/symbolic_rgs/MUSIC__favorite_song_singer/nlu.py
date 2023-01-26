10f896336a2b930a399cf04d38d76681113dd4e5
thanawan-atc <thanawan.atc@gmail.com>
2023-01-25 17:48:26 -0800

    Merge with Katherine + Clean up

--- chirpy/symbolic_rgs/MUSIC__favorite_song_singer/nlu.py
+++
@@ -1,9 +0,0 @@
-from chirpy.core.response_generator.nlu import nlu_processing
-
-@nlu_processing
-def get_flags(context):
-    return
-
-@nlu_processing
-def get_background_flags(context):
-    return
