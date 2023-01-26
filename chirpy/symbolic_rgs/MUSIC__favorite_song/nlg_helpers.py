7d6e1f0d42c42e778ca5647bf156379caf28a594
thanawan-atc <thanawan.atc@gmail.com>
2023-01-25 17:48:34 -0800

    Merge with Katherine + Clean up

--- chirpy/symbolic_rgs/MUSIC__favorite_song/nlg_helpers.py
+++ chirpy/symbolic_rgs/MUSIC__favorite_song/nlg_helpers.py
@@ -1,10 +1,4 @@
-import random
-import re
-
 from chirpy.core.response_generator import nlg_helper
-
-import chirpy.response_generators.music.response_templates.general_templates as templates
-from chirpy.response_generators.wiki2.wiki_utils import get_til_title
 
 @nlg_helper
 def get_plural_form(work_descriptor):
--- chirpy/symbolic_rgs/MUSIC__favorite_song/nlu.py
+++ chirpy/symbolic_rgs/MUSIC__favorite_song/nlu.py
@@ -1,14 +1,9 @@
 from chirpy.core.response_generator.nlu import nlu_processing
-
 from chirpy.response_generators.music.utils import WikiEntityInterface
 from chirpy.core.entity_linker.entity_groups import ENTITY_GROUPS_FOR_EXPECTED_TYPE
 from chirpy.core.entity_linker.entity_linker_simple import link_span_to_entity
-from chirpy.response_generators.wiki2.wiki_utils import get_til_title
 from chirpy.response_generators.music.regex_templates import NameFavoriteSongTemplate
 import re
-
-from chirpy.response_generators.music.utils import MusicBrainzInterface
-from chirpy.response_generators.music.music_helpers import tags
 
 def get_song_and_singer_entity(context):
     def is_song(ent):
@@ -36,18 +31,6 @@ def get_song_and_singer_entity(context):
 def get_song_entity(context, string):
     return link_span_to_entity(string, context.state_manager.current_state, expected_type=ENTITY_GROUPS_FOR_EXPECTED_TYPE.musical_work)
 
-# def get_song_meta(song_name, singer_name=None):
-#     if ' by ' in song_name:
-#         song_name, singer_name = song_name.split(' by ')
-#     musicbrainz = MusicBrainzInterface()
-#     metadata = musicbrainz.get_song_meta(song_name, singer_name)
-#     if metadata:
-#         for t, talkable_t in tags.items():
-#             if t in metadata['tags']:
-#                 metadata['tags'] = [talkable_t]
-#                 return metadata
-#     return metadata
-
 @nlu_processing
 def get_flags(context):
     song_ent, singer_ent = get_song_and_singer_entity(context)
@@ -56,23 +39,12 @@ def get_flags(context):
     if song_ent:
         song_str = song_ent.talkable_name
         song_str = re.sub(r'\(.*?\)', '', song_str)
-        # metadata = get_song_meta(song_str, singer_ent.talkable_name if singer_ent else None)
     else:
         song_slots = NameFavoriteSongTemplate().execute(context.utterance)
         if song_slots is not None and 'favorite' in song_slots:
             song_str = song_slots['favorite']
             song_ent = get_song_entity(context, song_str)
-            # metadata = get_song_meta(song_str, None)
 
-    # if metadata:
-    #     ADD_NLU_FLAG('MUSIC__song_metadata_exists')
-    #     ADD_NLU_FLAG('MUSIC__song_metadata_song', metadata["song"])
-    #     ADD_NLU_FLAG('MUSIC__song_metadata_artist', metadata["artist"])
-    #     ADD_NLU_FLAG('MUSIC__song_metadata_year', metadata["year"])
-    #     ADD_NLU_FLAG('MUSIC__singer_of_fav_song', metadata["artist"])
-    # else:
-    #     ADD_NLU_FLAG('MUSIC__singer_of_fav_song', None)
-    ADD_NLU_FLAG('MUSIC__singer_of_fav_song', "Taylor Swift")  # TODO: get singer using musicbrainz
 
     ADD_NLU_FLAG('MUSIC__fav_song_ent', song_ent)
     ADD_NLU_FLAG('MUSIC__fav_song_str', song_str)
